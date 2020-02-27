57fbea177edeb3b02fb3f0d8a3291f1a19dfa010
SharkovKirill <kirill.office75@gmail.com>
2020-02-27 13:39:25 +0700

    todo+defs

--- filesystem.py
+++ filesystem.py
@@ -19,14 +19,43 @@ def acceptCommand():
         return com
 
 
+# TODO:(Sveta) defs: countBytes(path), moveUp()
+# TODO:(Vladimir) defs: countFiles(path), moveDown(currentDir)
+
+def countBytes(path):
+    pass
+def moveUp():
+    pass
+def countFiles(path):
+    pass
+def moveDown(currentDir):
+    pass
+def findFiles(target,path):
+    pass
+
+def runCommand(command):
+    if command == 1:
+        os.listdir(os.getcwd())
+    elif command == 2:
+        moveUp()
+    elif command == 3:
+        moveDown(currentDir)
+    elif command == 4:
+        countFiles(path)
+    elif command == 5:
+        countBytes(path)
+    elif command == 6:
+        findFiles(target, path)
+    elif command == 7:
+        print(loc.quit)
+
+
 def main():
     while True:
         print(os.getcwd())
         print(loc.MENU)
         command = acceptCommand()
-        #runCommand(command)
-        #if command == QUIT:
-        #    print(loc.quit)
-#    break
-
+        runCommand(command)
+        if command ==7:
+            break
 main()
