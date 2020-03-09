89463e5ca5470a8cc81365718d19d498f92b53e4
Vladimir-Ermolenko <v.ermolenko@g.nsu.ru>
2020-03-09 15:28:32 +0700

    moves added

--- filesystem.py
+++ filesystem.py
@@ -1,4 +1,5 @@
 import os
+
 '''
 language = input('Change language: en, ru').lower()
 if language == 'en':
@@ -6,7 +7,9 @@ if language == 'en':
 elif language == 'ru':
     import locru as loc
 '''
+
 import locru as loc
+
 
 def acceptCommand():
     try:
@@ -19,19 +22,38 @@ def acceptCommand():
         return com
 
 
-# TODO:(Sveta) defs: countBytes(path), moveUp()
-# TODO:(Vladimir) defs: countFiles(path), moveDown(currentDir)
+def countBytes():
+    try:
+        if not os.listdir(os.getcwd()):
+            return
+        return os.path.getsize(os.getcwd())
+    except:
+        return acceptCommand()
 
-def countBytes(path):
-    pass
+
 def moveUp():
-    pass
+    try:
+        return os.chdir(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
+    except:
+        return acceptCommand()
+
+
 def countFiles(path):
     pass
-def moveDown(currentDir):
-    pass
+
+
+def moveDown():
+
+    print('\n' + 'Choose a directory to get to:' + '\n')
+    for n in range(len(os.listdir(os.getcwd()))):
+        print(str(n + 1) + '. ' + str(os.listdir(os.getcwd())[n]))
+    togo = input()
+    os.chdir(os.getcwd() + '/' + os.listdir(os.getcwd())[int(togo)])
+
+
 def findFiles(target,path):
     pass
+
 
 def runCommand(command):
     if command == 1:
@@ -39,11 +61,11 @@ def runCommand(command):
     elif command == 2:
         moveUp()
     elif command == 3:
-        moveDown(currentDir)
+        moveDown()
     elif command == 4:
         countFiles(path)
     elif command == 5:
-        countBytes(path)
+        print(countBytes())
     elif command == 6:
         findFiles(target, path)
     elif command == 7:
@@ -56,6 +78,8 @@ def main():
         print(loc.MENU)
         command = acceptCommand()
         runCommand(command)
-        if command ==7:
+        if command == 7:
             break
+
+
 main()
